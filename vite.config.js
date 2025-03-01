c828cdb1767fb1a1dff4b6acd15706cf242768a1
Andreas Zahnen <zahnen@interactive-instruments.de>
2025-03-01 13:21:10 +0100

    fix example

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       name: "OgcApiEditor",
       formats: ["umd"],
       //fileName: `ol-ogc-api-crud.[hash]`,
-      fileName: `ol-ogc-api-crud`,
+      fileName: () => `ol-ogc-api-crud.js`,
     },
     rollupOptions: {
       external: (path) => /ol\//.test(path) || path === "ol-mapbox-style",
